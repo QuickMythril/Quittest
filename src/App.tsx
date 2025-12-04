04ee17c0eaaf6ac26667d13d0ca0f470c14aa8d0
QuickMythril <quickmythril@protonmail.com>
2025-12-03 20:58:05 -0500

    Pass avatar to post inputs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import { SocialApp } from './components/SocialApp';
 function App() {
   const { auth } = useGlobal();
 
-  return <SocialApp userName={auth?.name || 'User'} />;
+  return (
+    <SocialApp
+      userName={auth?.name || 'User'}
+      userAvatar={auth?.avatarUrl || undefined}
+    />
+  );
 }
 
 export default App;
